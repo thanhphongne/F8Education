e817435b525866c9f1763fbb088517e4c02270cd
etnad17 <dangtran151@gmail.com>
2021-12-08 12:31:31 +0700

    debug upload

--- src/app/controllers/QuestionController.js
+++ src/app/controllers/QuestionController.js
@@ -1,33 +1,33 @@
-const Question = require('../models/Question');
-const { mutipleMongooseToObject } = require('../../util/mongoose');
-
+const Question = require("../models/Question");
+const { mutipleMongooseToObject } = require("../../util/mongoose");
 
 class QuestionController {
-    //[POST] /question/store
-    store(req, res, next) {
-        req.body.image = req.file.path;
-        const question = new Question(req.body);
-        question
-            .save()
-            .then(() => res.redirect('/question'))
-            .catch(next);
-    }
-    //[GET] /question
-    index(req, res, next) {
-        Question.find({})
-            .then((questions) => {
-                res.render('question/question', {
-                    questions: mutipleMongooseToObject(questions),
-                });
-            })
-            .catch(next);
-    }
-    //[PUT] /question/:id
-    answer(req, res, next) {
-        Question.updateOne({ _id: req.params.id }, req.body)
-            .then(() => res.redirect('/question'))
-            .catch(next);
-    }
+  //[POST] /question/store
+  store(req, res, next) {
+    req.body.image = req.file.path.split("\\").slice(2).join("/");
+
+    const question = new Question(req.body);
+    question
+      .save()
+      .then(() => res.redirect("/question"))
+      .catch(next);
+  }
+  //[GET] /question
+  index(req, res, next) {
+    Question.find({})
+      .then((questions) => {
+        res.render("question/question", {
+          questions: mutipleMongooseToObject(questions),
+        });
+      })
+      .catch(next);
+  }
+  //[PUT] /question/:id
+  answer(req, res, next) {
+    Question.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect("/question"))
+      .catch(next);
+  }
 }
 
 module.exports = new QuestionController();
--- src/routes/question.js
+++ src/routes/question.js
@@ -1,14 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const multer  = require('multer');
+const multer = require("multer");
 
+const upload = multer({ dest: "./src/public/uploads/" });
 
-const upload = multer({ dest: 'uploads/' });
+const questionController = require("../app/controllers/QuestionController");
 
-const questionController = require('../app/controllers/QuestionController');
-
-router.put('/:id', questionController.answer);
-router.post('/store',upload.single('image'), questionController.store);
-router.get('/', questionController.index);
+router.put("/:id", questionController.answer);
+router.post("/store", upload.single("image"), questionController.store);
+router.get("/", questionController.index);
 
 module.exports = router;
